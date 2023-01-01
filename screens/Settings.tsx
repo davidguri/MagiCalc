3d8624c4075798a963e6800d63009aab4b5ca4eb
davidguri <david.guri@icloud.com>
2023-01-01 13:23:04 +0100

    Trying to figure out how to access contacts

--- screens/Settings.tsx
+++ screens/Settings.tsx
@@ -1,14 +1,18 @@
 import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
 import {
-    StyleSheet,
-    Text,
-    View,
-    SafeAreaView,
-    TouchableOpacity,
-    Button,
-    TextInput,
+   StyleSheet,
+   Text,
+   View,
+   ScrollView,
+   SafeAreaView,
+   TouchableOpacity,
+   Button,
+   TextInput,
 } from "react-native";
-import Modal from "react-native-modal"
+import Modal from "react-native-modal";
+import ListItem from "../components/ListItem";
+
 import Contacts from "react-native-contacts";
 import Ionicons from '@expo/vector-icons/Ionicons';
 
@@ -18,77 +22,93 @@ import Ionicons from '@expo/vector-icons/Ionicons';
 // in the modal, you can search for a certain contact from your contacts,
 // and their number will be used as the output in the calculator.
 
-export default function SettingsPage(props) {
-    var searchText = "Search..."
-    return (
-        <Modal
-            isVisible={props.visible}
-            animationIn={"slideInUp"}
-            animationOut={"slideOutDown"}
-            style={{ margin: 0 }}
-            hideModalContentWhileAnimating={true}
-        >
-            <SafeAreaView style={styles.container}>
-                <StatusBar style="light" />
-                <View style={styles.bodyContainer}>
-                    <View style={styles.topContainer}>
-                        <Button title="Done" color="#4fffe3" onPress={props.onCancel} />
-                        <View style={styles.searchCont}>
-                            <Ionicons name="search-outline" size={18} color="#aaa" />
-                            <TextInput style={styles.searchText} keyboardAppearance="dark" value={searchText} onFocus={() => searchText = ""} />
-                        </View>
-                    </View>
-                    <View style={styles.bottomContainer}></View>
-                </View>
-            </SafeAreaView>
-        </Modal>
-    );
+export default function SettingsPage(props: any) {
+   const [isSearchText, setIsSearchText] = useState("Search...");
+
+   return (
+      <Modal
+         isVisible={props.visible}
+         animationIn={"slideInUp"}
+         animationOut={"slideOutDown"}
+         style={{ margin: 0 }}
+         hideModalContentWhileAnimating={true}
+      >
+         <SafeAreaView style={styles.container}>
+            <StatusBar style="light" />
+            <View style={styles.bodyContainer}>
+               <View style={styles.topContainer}>
+                  <Button title="Done" color="#4fffe3" onPress={props.onCancel} />
+                  <View style={styles.searchCont}>
+                     <Ionicons name="search-outline" size={18} color="#aaa" />
+                     <TextInput style={styles.searchText} keyboardAppearance="dark" value={isSearchText} onFocus={() => setIsSearchText("")} />
+                  </View>
+               </View>
+               <ScrollView style={styles.bottomContainer}>
+                  <TouchableOpacity onPress={() => { }}>
+                     <ListItem style={styles.listItem}>
+                        <Ionicons name="person-circle-outline" size={40} color="black" />
+                        <Text style={styles.listText}>Hello</Text>
+                     </ListItem>
+                  </TouchableOpacity>
+               </ScrollView>
+            </View>
+         </SafeAreaView>
+      </Modal>
+   );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#000",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-    },
+   container: {
+      flex: 1,
+      backgroundColor: "#000",
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "center",
+   },
 
-    bodyContainer: {
-        flex: 1,
-        width: "100%",
-    },
+   bodyContainer: {
+      flex: 1,
+      width: "100%",
+   },
 
-    topContainer: {
-        borderColor: "red",
-        borderWidth: 2,
-        paddingBottom: 15,
-    },
+   topContainer: {
+      paddingBottom: 15,
+   },
 
-    bottomContainer: {
-        borderColor: "green",
-        borderWidth: 2,
-        flex: 1,
-    },
+   bottomContainer: {
+      flex: 1,
+   },
 
-    button: {},
+   button: {},
 
-    searchCont: {
-        borderColor: "#4fffe3",
-        borderWidth: 4,
-        borderRadius: 25,
-        width: "95%",
-        marginHorizontal: "2.5%",
-        marginVertical: 12,
-        flexDirection: "row",
-        alignItems: "center",
-        paddingHorizontal: 15,
-    },
+   searchCont: {
+      borderColor: "#4fffe3",
+      borderWidth: 4,
+      borderRadius: 25,
+      width: "96%",
+      marginHorizontal: "2%",
+      marginVertical: 12,
+      flexDirection: "row",
+      alignItems: "center",
+      paddingHorizontal: 15,
+   },
 
-    searchText: {
-        color: "#aaa",
-        fontSize: 18,
-        paddingVertical: 12,
-        paddingHorizontal: 6,
-    },
+   searchText: {
+      color: "#aaa",
+      fontSize: 18,
+      paddingVertical: 12,
+      paddingHorizontal: 6,
+      flex: 1,
+   },
+
+   listItem: {
+      flexDirection: "row",
+      alignItems: "center",
+   },
+
+   listText: {
+      fontSize: 25,
+      fontWeight: "500",
+      paddingLeft: 10,
+   },
 });
